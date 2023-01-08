13c15d4a590f975e563ec31cd64ec9d6f23a4ce7
Bad-Karmaa <caoviethoang.lfay@gmail.com>
2023-01-08 12:19:16 +0100

    mettre en commentaire em

---
+++ src/Controller/Admin/AdminBienController.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Controller\Admin;
+
+use App\Entity\Bien;
+use App\Form\BienType;
+use App\Repository\BienRepository;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/bien", name="admin_bien_")
+ * @package App\Controller\Admin
+ */
+class AdminBienController extends AbstractController
+{
+    /**
+     * @var BienRepository
+     */
+    private BienRepository $repository;
+
+    /**
+     * @var ObjectManager
+     */
+    private ObjectManager $em;
+
+    /*public function __construct(BienRepository $repository, ObjectManager $em)
+    {
+        $this -> repository = $repository;
+        $this -> em = $em;
+    }*/
+
+    /**
+     * @Route("/admin", name="home")
+     */
+    public function index(BienRepository $bienRepo)
+    {
+        return $this->render('admin/bien/index.html.twig', [
+            'bien' => $bienRepo->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ajout", name="admin.bien.ajout", methods="GET|POST")
+     */
+    public function ajoutBien(Request $request)
+    {
+        $bien = new Bien;
+
+        $form = $this->createForm(BienType::class, $bien);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($bien);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.bien.index');
+        }
+
+        return $this->render('admin/bien/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("admin/modifier/{id}", name="admin.bien.modifier")
+     */
+    public function ModifCategorie(Bien $bien, Request $request)
+    {
+        $form = $this->createForm(BienType::class, $bien);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($bien);
+            $em->flush();
+
+            return $this->redirectToRoute('admin.bien.index');
+        }
+
+        return $this->render('admin/bien/ajout.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
