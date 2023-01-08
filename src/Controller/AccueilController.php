ee242a4a7ca76714636b1b8a106b0085ac6e7e99
Thais Amegbedji <tamegbedji@univ-rennes1.fr>
2023-01-09 00:38:00 +0100

    Corrections de message d'erreur

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -15,10 +15,17 @@ class AccueilController extends AbstractController
     #[Route('/accueil', name: 'app_accueil')]
     public function index(CategoriesRepository $categoriesRepository, ManagerRegistry $managerRegistry): Response
     {
+        // Récupérer toutes les catégories de la base de données sur CategoriesRepository
         $categories = $categoriesRepository->findAll();
+
+        // Sélectionner trois biens aléatoirement dans la table 'biens'
         $sql = 'SELECT * from biens ORDER BY RAND() LIMIT 3';
+
+        // Récupérer l'entity manager et préparer la requête SQL précédente
         $em = $managerRegistry->getManager();
         $statement = $em->getConnection()->prepare($sql);
+
+        // Exécuter la requête et récupérer les résultats
         $result = $statement->execute()->fetchAll();
         $biens = [];
 
